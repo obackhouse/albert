3bc5b8d846f305ac29d9b7e9a188b3c1289d67c3
Oliver Backhouse <olbackhouse@gmail.com>
2024-02-27 18:46:24 +0000

    Extra wick constructors not in original package

--- albert/qc/_wick.py
+++ albert/qc/_wick.py
@@ -9,13 +9,13 @@ try:
     from qwick.convenience import commute, ep11, one_e, one_p, two_e, two_p
     from qwick.expression import Expression, Term
     from qwick.index import Idx
-    from qwick.operator import BOperator, FOperator, Sigma, Tensor, TensorSym
+    from qwick.operator import Projector, BOperator, FOperator, Sigma, Tensor, TensorSym
     from qwick.wick import apply_wick  # noqa: F401
 except ImportError:
     from wick.convenience import commute, ep11, one_e, one_p, two_e, two_p
     from wick.expression import Expression, Term
     from wick.index import Idx
-    from wick.operator import BOperator, Sigma, Tensor, FOperator, TensorSym
+    from wick.operator import Projector, BOperator, Sigma, Tensor, FOperator, TensorSym
     from wick.wick import apply_wick  # noqa: F401
 
 from albert.algebra import Add, Mul
@@ -203,12 +203,12 @@ def _convert_symbol(symbol, index_spins=None):
 
     elif re.match(r"lu11_\{[a-z][a-z][a-z]\}", symbol):
         # lu11_{Iij}
-        indices = tuple(symbol[5:8])
+        indices = tuple(symbol[6:10])
         tensor_symbol = LU11
 
     elif re.match(r"lu12_\{[a-z][a-z][a-z][a-z]\}", symbol):
         # lu12_{IJij}
-        indices = tuple(symbol[5:9])
+        indices = tuple(symbol[6:10])
         tensor_symbol = LU12
 
     elif re.match(r"u11_\{[a-z][a-z][a-z]\}", symbol):
@@ -681,3 +681,73 @@ def bch(h, t, max_commutator=4):
 
 
 construct_hbar = bch
+
+
+def P_dexit1(name, spaces, index_key=None):
+    """
+    Return the tensor representation of a Boson de-excitation operator.
+    """
+    terms = []
+    for s in spaces:
+        x = Idx(0, s, fermion=False)
+        sums = [Sigma(x)]
+        tensors = [Tensor([x], name)]
+        operators = [BOperator(x, False)]
+        e1 = Term(1, sums, tensors, operators, [], index_key=index_key)
+        terms.append(e1)
+    return Expression(terms)
+
+
+def EPS_dexit1(name, bspaces, ospaces, vspaces, index_key=None):
+    """
+    Return the tensor representation of a coupled Fermion excitation
+    operator and Boson deexcitation operator.
+    """
+    terms = []
+    for bs in bspaces:
+        for os in ospaces:
+            for vs in vspaces:
+                x = Idx(0, bs, fermion=False)
+                i = Idx(0, os)
+                a = Idx(0, vs)
+                sums = [Sigma(x), Sigma(i), Sigma(a)]
+                tensors = [Tensor([x, a, i], name)]
+                operators = [BOperator(x, False), FOperator(a, True), FOperator(i, False)]
+                e1 = Term(1, sums, tensors, operators, [], index_key=index_key)
+                terms.append(e1)
+    return Expression(terms)
+
+
+def PB1(bspace, index_key=None):
+    """
+    Return the projector onto a space of single boson occupations.
+    """
+    x = Idx(0, bspace, fermion=False)
+    P = Projector()
+    operators = [BOperator(x, True), P, BOperator(x, False)]
+    exp = Expression([Term(1, [Sigma(x)], [], operators, [], index_key=index_key)])
+    return exp
+
+
+def PB2(bspace, index_key=None):
+    """
+    Return the projector onto a space of double boson occupations.
+    """
+    x = Idx(0, bspace, fermion=False)
+    y = Idx(1, bspace, fermion=False)
+    P = Projector()
+    operators = [BOperator(x, True), BOperator(y, True), P, BOperator(y, False), BOperator(x, False)]
+    exp = Expression([Term(1, [Sigma(x), Sigma(y)], [], operators, [], index_key=index_key)])
+    return exp
+
+
+def PE2(ospace, vspace, index_key=None):
+    """
+    Return the projector onto a space of double fermion excitations.
+    """
+    i = Idx(0, ospace)
+    a = Idx(0, vspace)
+    P = Projector()
+    operators = [FOperator(a, True), FOperator(i, False), P, FOperator(i, True), FOperator(a, False)]
+    exp = Expression([Term(1, [Sigma(i), Sigma(a)], [], operators, [], index_key=index_key)])
+    return exp
